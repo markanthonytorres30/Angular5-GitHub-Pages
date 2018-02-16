725dcfeb19cadc9b5a6ea989bd0c66c8844d6583
Mark Anthony <markanthonytorres30@gmail.com>
2018-02-16 21:15:14 +0800

    added one-way and two-way bindiing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4;
+  btnText: string = 'Add an Item';
+  goalText: string = 'My first life goal';
+
   constructor() { }
 
   ngOnInit() {
